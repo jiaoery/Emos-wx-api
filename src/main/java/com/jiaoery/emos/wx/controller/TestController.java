a18f496098f291faea798f5d559636ef4d470e31
jiaoery <cqjixiang@gmail.com>
2022-06-20 17:59:53 +0800

    集成swagger和配置后端valid验证

--- src/main/java/com/jiaoery/emos/wx/controller/TestController.java
+++ src/main/java/com/jiaoery/emos/wx/controller/TestController.java
@@ -9,21 +9,22 @@ package com.jiaoery.emos.wx.controller;
  * @Date 2022/6/20 15:22
  */
 import com.jiaoery.emos.wx.common.util.R;
+import com.jiaoery.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/test")
 @Api("测试Web接口")
 public class TestController {
 
-    @GetMapping("/sayHello")
+    @PostMapping("/sayHello")
     @ApiOperation("最简单的测试方法")
-    public R sayHello(){
-        return R.ok().put("message","HelloWorld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form){
+        return R.ok().put("message","Hello,"+form.getName());
     }
 }
 
---
+++ src/main/java/com/jiaoery/emos/wx/controller/form/TestSayHelloForm.java
@@ -0,0 +1,25 @@
+package com.jiaoery.emos.wx.controller.form;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/**
+ * ClassName:TestSayHelloForm
+ * Description:
+ *
+ * @author jixiang
+ * @version v1.0.0
+ * @Date 2022/6/20 16:16
+ */
+@ApiModel
+@Data
+public class TestSayHelloForm {
+    @NotBlank(message = "姓名不可为空")
+    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "输入姓名不符合规范")
+    @ApiModelProperty("姓名")
+    private String name;
+}

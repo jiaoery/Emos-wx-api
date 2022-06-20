827677ec39c49096ab337f7d234c1fd4ab7de56b
jiaoery <cqjixiang@gmail.com>
2022-06-20 19:35:58 +0800

    抵御xss跨站脚本攻击

---
+++ src/main/java/com/jiaoery/emos/wx/config/xss/XssFilter.java
@@ -0,0 +1,30 @@
+package com.jiaoery.emos.wx.config.xss;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * ClassName:XssFilter
+ * Description:
+ *
+ * @author jixiang
+ * @version v1.0.0
+ * @Date 2022/6/20 19:25
+ */
+@WebFilter(urlPatterns = "/*")
+public class XssFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        XssHttpServletRequestWrapper xssRequest = new XssHttpServletRequestWrapper(
+                (HttpServletRequest) servletRequest);
+        filterChain.doFilter(xssRequest, servletResponse);
+    }
+}
--- src/main/java/com/jiaoery/emos/wx/controller/form/TestSayHelloForm.java
+++ src/main/java/com/jiaoery/emos/wx/controller/form/TestSayHelloForm.java
@@ -19,7 +19,7 @@ import javax.validation.constraints.Pattern;
 @Data
 public class TestSayHelloForm {
     @NotBlank(message = "姓名不可为空")
-    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "输入姓名不符合规范")
+//    @Pattern(regexp = "^[\\u4e00-\\u9fa5]{2,15}$",message = "输入姓名不符合规范")
     @ApiModelProperty("姓名")
     private String name;
 }
